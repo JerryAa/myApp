04d02a38e0a07c17a6ee89807fa00fa5bd16af88
JerryAa <jerrya15@yahoo.com>
2018-08-25 02:05:23 -0500

    added index

--- new.cs
+++ new.cs
@@ -4,12 +4,41 @@ namespace myApp
 { 
 
 	public class D
-	{ 
+	{	
+
+
+		private int [] arr = new int[10]; 
 
 		public void Run() 
 		{ 
-
 			Console.WriteLine("Inside Run()"); 
+		} 
+		
+		public int this[int index]
+		{ 
+			get	
+			{ 
+				if (index < 0 && index >= arr.Length) 
+				{ 
+					throw new IndexOutOfRangeException("Index is out of range");	
+				} 		
+				else 
+				{ 
+					return arr[index]; 
+				} 
+
+			} 
+			set 
+			{ 
+				if (index < 0 && index >= arr.Length) 
+					throw new IndexOutOfRangeException("Index is out of range");	
+				else 
+				{ 
+					arr[index] = value;
+				} 
+			
+			} 
+
 		} 
 
 	} 
